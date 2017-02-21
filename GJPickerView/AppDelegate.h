73d248618dcb2a59337686d20c55e5ef76c9d099
gejiangs <664553782@qq.com>
2017-02-21 10:44:12 +0800

    Initial Commit

---
+++ GJPickerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GJPickerView
+//
+//  Created by 郭江 on 2017/2/21.
+//  Copyright © 2017年 郭江. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GJPickerView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GJPickerView
+//
+//  Created by 郭江 on 2017/2/21.
+//  Copyright © 2017年 郭江. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
